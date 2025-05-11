92cee8609dee922bc581d2d7889f925d0bfca2a7
fadeldnswr <fadel.achmad@ui.ac.id>
2025-05-11 11:27:39 +0700

    create ping server function

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <PubSubClient.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <ESP32Ping.h>
 
 // Define sensor type and pin
 #define DHTTYPE DHT22
@@ -18,18 +19,22 @@ const char* mqttServer = "Raspberry Pi Ip"; // Change into Raspberry Pi IP
 WiFiClient espClient;
 PubSubClient client(espClient);
 DHT dht(sensorPin, DHTTYPE);
+IPAddress ip(192, 168, 1, 12); // Change into Raspberry Pi IP Address
 
 // Define global variable for metrics measurement
 unsigned long lastMillis = 0; // Last time data was sent
 unsigned long latency = 0; // Store latency data
-unsigned long throughput = 0; // Store throughput
-float powerConsumption = 0.0; // Power consumption in mA
+unsigned long throughput = 0; // Store throughput data
+
+void setupWifi();
+void pingServer();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("DHT22 Start!");
 
+  // Configure setup for wifi, MQTT connection and sensors
+  setupWifi();
   client.setServer(mqttServer, 1883);
   dht.begin();
 }
@@ -38,13 +43,19 @@ void loop() {
   // put your main code here, to run repeatedly:
   delay(2000);
 
+  // Ping the server
+  pingServer();
+}
+
+// Create read sensor function
+void readSensorData(){
   // Read temperature
   float temp = dht.readTemperature();
 
   // Read humidity
   float hum = dht.readHumidity();
 
-  // Check if no data send
+  // Check if no data being sended
   if(isnan(hum) || isnan(temp)){
     Serial.println("Failed to read temperature and humidity data from sensor!");
     return;
@@ -60,6 +71,25 @@ void loop() {
   Serial.println(hum);
 }
 
+// Create ping function
+void pingServer(){
+  // Ping the emas2.ui.ac.id 4 times
+  Serial.println("Pinging the server...");
+  bool ret = Ping.ping(ip, 4);
+
+  // Check if ping is success
+  if(ret){
+  
+  // Check the avg response time
+  float avg_res_time_ms = Ping.averageTime();
+  Serial.print("Average response time: ");
+  Serial.print(avg_res_time_ms);
+  Serial.println(" ms");
+  } else {
+    Serial.println("Ping failed!");
+  }
+}
+
 // WiFi Setup function
 void setupWifi(){
   delay(10);
@@ -67,6 +97,8 @@ void setupWifi(){
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
+  Serial.print("WiFi Ip address: ");
+  Serial.println(WiFi.localIP());
 }
 
 // Reconnect function
