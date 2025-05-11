3ec4ff82dd5d05c52199754ff749ee2cca1cc877
fadeldnswr <fadel.achmad@ui.ac.id>
2025-05-11 21:39:16 +0700

    revise the publish and subscribe method

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,6 @@
 #include <DHT.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <ESP32Ping.h>
 
 // Define sensor type and pin
 #define DHTTYPE DHT22
@@ -13,41 +10,26 @@ int sensorPin = 18;
 // WiFi configuration
 const char* ssid = "odoxs";
 const char* password = "kingking04";
-const char* mqttServer = "Raspberry Pi Ip"; // Change into Raspberry Pi IP
+const char* mqttServer = "192.168.1.14"; // Change into Raspberry Pi IP
 
 // Define struct configuration
 WiFiClient espClient;
 PubSubClient client(espClient);
 DHT dht(sensorPin, DHTTYPE);
-IPAddress ip(192, 168, 1, 12); // Change into Raspberry Pi IP Address
 
-// Define global variable for metrics measurement
-unsigned long lastMillis = 0; // Last time data was sent
-unsigned long latency = 0; // Store latency data
-unsigned long throughput = 0; // Store throughput data
-
-void setupWifi();
-void pingServer();
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-
-  // Configure setup for wifi, MQTT connection and sensors
-  setupWifi();
-  client.setServer(mqttServer, 1883);
-  dht.begin();
+void reconnect(){
+  // Loop until connected
+  while(!client.connected()){
+    if(client.connect("ESP32Client")){
+      Serial.println("Connected to MQTT broker");
+    } else {
+      delay(5000);
+      Serial.print(".");
+    }
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  delay(2000);
-
-  // Ping the server
-  pingServer();
-}
-
-// Create read sensor function
+// Create read sensor function and publish data into MQTT broker
 void readSensorData(){
   // Read temperature
   float temp = dht.readTemperature();
@@ -61,53 +43,35 @@ void readSensorData(){
     return;
   }
 
-  // Read temperature
-  Serial.print("Temperature : ");
-  Serial.print(temp);
-  Serial.println(" C");
+  // Publish temperature and humidity data
+  String payload = String("Temperature: ") + String(temp) + " C, Humidity: " + String(hum) + " %";
+  client.publish("iot/data", payload.c_str());
 
-  // Read humidity
-  Serial.print("Humidity : ");
-  Serial.println(hum);
+  Serial.print("Data sent to MQTT broker: ");
+  Serial.println(payload);
+
+  delay(2000);
 }
 
-// Create ping function
-void pingServer(){
-  // Ping the emas2.ui.ac.id 4 times
-  Serial.println("Pinging the server...");
-  bool ret = Ping.ping(ip, 4);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
 
-  // Check if ping is success
-  if(ret){
-  
-  // Check the avg response time
-  float avg_res_time_ms = Ping.averageTime();
-  Serial.print("Average response time: ");
-  Serial.print(avg_res_time_ms);
-  Serial.println(" ms");
-  } else {
-    Serial.println("Ping failed!");
-  }
-}
-
-// WiFi Setup function
-void setupWifi(){
-  delay(10);
+  // Configure setup for wifi, MQTT connection and sensors
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.println("Connecting to WiFi...");
+    delay(1000);
   }
-  Serial.print("WiFi Ip address: ");
-  Serial.println(WiFi.localIP());
+  client.setServer(mqttServer, 1883);
+  dht.begin();
 }
 
-// Reconnect function
-void reconnect(){
-  while (!client.connected()){
-    if(client.connect("ESP32Client")) {
-      client.subscribe("esp32/data");
-    } else {
-      delay(5000);
-    }
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(!client.connected()){
+    reconnect();
   }
+  client.loop();
+  readSensorData();
 }
